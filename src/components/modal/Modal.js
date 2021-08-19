d0098efb2c9e5ed1f3d1048b2b747e4c224de25e
KaarinaTamara <kaarina.angus@gmail.com>
2021-08-19 11:37:28 -0400

    fixed layout issue on list section, now everything lines up. added ability to save comments on modal. still no name

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,10 +1,33 @@
 // Modal built with code-along by YouTube user Cand Dev at https://www.youtube.com/watch?v=sFCHGba2OEo
 
 import React from 'react'; 
+import {useState} from 'react';
+
 // import { programObject } from './App.js';
 
-
 export const Modal = ({ show, close }) => {
+    const [inputSubmittedComment, setInputSubmittedComment] = useState("");
+
+    const [commentInput, setCommentInput] = useState("");
+    const handleChange = (e) => {
+        if (e.target.name === "userInformation") {
+            setCommentInput(e.target.value) 
+        }        
+    }
+
+    //How to handle submitted comments
+    const handleClick = (e) => {
+        e.preventDefault() 
+        if (commentInput === "") {
+            alert("Please enter a comment before submitting")
+        } else {
+            let storedComment = inputSubmittedComment
+            setInputSubmittedComment(commentInput + storedComment)
+            setCommentInput("")
+        } 
+
+    }
+
     return (
         // BUILDING OUR MODAL
         <div className="modal-wrapper"
@@ -21,21 +44,26 @@ export const Modal = ({ show, close }) => {
             <div className="modal-content">
                 <div className="modal-body">
                     <h2>Tell Katsella your secrets</h2>
-                    {/* <h2>{programObject.title}</h2> */}
+                    
                     <p>Leave a comment here for Katsella about this choice . Are you excited to watch it? Did you recommend it for a particular reason? This is your space and Katsella wants to know it all! </p>
                     <form className="modal-form">
 
-                        <label for="userInformation">Listen up, Katsella: </label>
-                        <textarea id="userInformation" name="userInformation" rows="8"></textarea>
-
-                        <button type="submit">Submit</button>
+                        <label htmlFor="userInformation">Listen up, Katsella: </label>
+                        <input type="text" className="name" placeholder="Name:"></input> 
+                        <textarea id="userInformation" name="userInformation" rows="8" className="userComment" value={commentInput} onChange={handleChange}></textarea>
+                        
+                        <button type="submit" onClick={handleClick}>Submit</button>
 
                     </form>
+                </div>
+                <div className="showComment">
+                    <p className="submittedComment">{inputSubmittedComment}</p>
                 </div>
                 <div className="modal-footer">
                     <button onClick={close} className="btn-cancel">Close</button>
                 </div>
             </div>
         </div>
+
     )
 };
